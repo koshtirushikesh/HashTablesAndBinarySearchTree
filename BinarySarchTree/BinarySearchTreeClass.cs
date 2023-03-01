4d359a3e4baede3322d6e4b7cdb2c1924da3257e
koshtirushikesh <koshtirushikesh3@gmail.com>
2023-03-01 11:26:12 +0530

    [Rushi]:create a BST by adding 56 and then adding 30 & 70

---
+++ BinarySarchTree/BinarySearchTreeClass.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace BinarySarchTree
+{
+    internal class BinarySearchTreeClass
+    {
+        Node root;
+        public void Insert(Node node)
+        {
+            root = InsertHelper(root, node);
+        }
+        public Node InsertHelper(Node root, Node node)
+        {
+            int data = node.data;
+            if (root == null)
+            {
+                root = node;
+                return root;
+            }
+            else if (data < root.data)
+            {
+                root.left = InsertHelper(root.left, node);
+            }
+            else
+            {
+                root.right = InsertHelper(root.right, node);
+            }
+            return root;
+        }
+        public void Display()
+        {
+            DisplayHelper(root);
+        }
+        public void DisplayHelper(Node root)
+        {
+            if (root != null)
+            {
+                DisplayHelper(root.left);
+                Console.Write(root.data + " ");
+                DisplayHelper(root.right);
+                return;
+            }
+        }
+    }
+}
---
+++ BinarySarchTree/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySarchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BinarySearchTreeClass tree = new BinarySearchTreeClass();
+            tree.Insert(new Node(56));
+            tree.Insert(new Node(30));
+            tree.Insert(new Node(70));
+            Console.Write("Binery Search Tree :");
+            tree.Display();
+            Console.ReadLine();
+        }
+    }
+}
